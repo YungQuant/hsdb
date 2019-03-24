0a2f8ceac6a7c5c31d1a9032598424b4ec50ec37
yungquant <theboss723@gmail.com>
2019-03-24 11:45:29 -0400

    fixed scraper, added 0.1 sec sleep as well

--- bitmex_scraper/src/run.sh
+++ bitmex_scraper/src/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "Compiling BitMEX System............"
+echo "Compiling BitMEX Scraper System............"
 echo "Removing existing file"
 rm -r ./trader
 echo "File removed, compiling..............."
-g++ -o trader trader.cpp bitmex.cpp feed.cpp data.cpp encrypt++/encoder.cpp -lcurl -lcpprest -lcrypto++ -lpthread -std=c++11 -I/usr/include/python2.7 -lpython2.7
-echo "BitMEX System Compiled............."
+g++ -o scraper trader.cpp bitmex.cpp feed.cpp data.cpp encrypt++/encoder.cpp -lcurl -lcpprest -lcrypto++ -lpthread -std=c++11 -I/usr/include/python2.7 -lpython2.7
+echo "BitMEX Scraper System Compiled............."
 exit 0
