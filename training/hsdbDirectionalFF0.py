2d846498ab405938de6293e1c8f4ca4044678774
yungquant <theboss723@gmail.com>
2019-04-01 18:05:31 -0400

    model restructure

--- training/hsdbDirectionalFF0.py
+++ training/hsdbDirectionalFF0.py
@@ -6,6 +6,7 @@ from sklearn import svm, linear_model
 from sklearn.metrics import mean_squared_error
 import keras
 from keras.callbacks import ReduceLROnPlateau
+from keras.utils import Sequence
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, BatchNormalization, LeakyReLU, PReLU
 from keras.layers import LSTM, Dropout
@@ -14,8 +15,47 @@ import numpy as np
 import time, datetime
 
 
+def writeDirectionalDataset(X, Y, path="../..HSDB_unnamedDataset.txt"):
+    path = path.split("_")[0] + "_" + str(datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%Z")) + ".txt"
+    fileP = open(path, "w")
+
+    for i in range(len(X)):
+        try:
+            strX, strY = "", ""
+            for k in range(len(X[i])):
+                strX += str(X[i][k])
+                strX += ","
+
+            strY = str(Y[i])
+
+            fileP.write(strX)
+            fileP.write(strY)
+
+        except Exception as e:
+            print(e)
+
+    fileP.close()
+
+def readDataset(path="../..HSDB_unnamedDataset.txt"):
+    fileP = open(path, "r")
+    lines = fileP.readlines()
+
+    X, Y = [], []
+
+    i = 0
+    while i < len(lines)-1:
+        linex = lines[i].split(",")
+        liney = lines[i+1].split(",")
+
+        X.append([float(l) for l in linex])
+        Y.append([float(l) for l in liney])
+
+        i+=2
+
+    return X, Y
+
 def forza(currency="XBTUSD", depth=30, p=1):
-    path = f'../HSDB_{currency}.txt'
+    path = f'../../HSDB_{currency}.txt'
     data, datum = [], []
     if os.path.isfile(path) == False:
         print(f'could not source {path} data')
@@ -82,29 +122,53 @@ def create_forzaFortuneTeller_dataset(dataset):
         dataY.append([dataset[i+1][0], dataset[i+1][1], dataset[i+1][2], dataset[i+1][3], dataset[i+1][60], dataset[i+1][61], dataset[i+1][62], dataset[i+1][63]])
     return np.array(dataX), np.array(dataY)
 
+
+class hsdbSequence(Sequence):
+
+    def __init__(self, x_set, y_set, batch_size):
+        self.x, self.y = x_set, y_set
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(np.ceil(len(self.x) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        return np.array(batch_x), np.array(batch_y)
+
 # TO-DO: write formatted datasets to file, read from files on training to save reformatting time
+# TO-DO: train in chunks to avoid overloading 8 GB GPU RAM (safe @ {Din = 30; dist = 100; perc = 0.2; c = 2} on 20 GB file)
+# TO_DO: mkdir models && mkdir models/models && mkdir models/training && FORMAT THE FUCKING FILEPATHS :(
+
+path = input("Enter data path:")
 
 timeStr = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
 currency_pairs, currencies = ["XBTUSD", "ETHUSD", "XRPU18", "LTCU18", "BCHU18"], ["BTCUSD", "ADABTC", "ETHUSD", "LTCBTC", "XRPBTC"]
-errs, passes, fails = [], 0, 0
-Din = 30; dist = 100; perc = 0.2; c = 1.5
+Dfiles = ["XBTUSD02"]
+errs, Ps, passes, fails = [], [], 0, 0
+Din = 30; dist = 666; perc = 1; c = 1.25; b = 100; nb_epoch = 25
 
-X, Y = create_forzaDirection_dataset(forza(currency_pairs[0], Din, perc), dist)
+X, Y = create_forzaDirection_dataset(forza(path, Din, perc), dist)
+writeDirectionalDataset(X, Y, f'../../HSDBdirectionalFF0-Din{Din}-dist{dist}-perc{perc}-cut{c}-dataset{path}-_thispartgetscut')
 print("X0: ", X[0], " Y0 ", Y[0], "mean/min/max(Y):", np.mean(Y), min(Y), max(Y))
 print("\nshape(X):", X.shape)
 
 trainX, trainY, testX, testY = X[:int(np.floor(len(X)/c))], Y[:int(np.floor(len(X)/c))], X[int(np.floor(len(X)/c)):], Y[int(np.floor(len(X)/c)):]
 # print(testX[0], testY[0])
 
-# scaler = MinMaxScaler(feature_range=(-1, 1))
+# scaler = MinMaxScaler(feature_range=(-10, 10))
 # trainX = scaler.fit_transform(trainX)
+# testX = scaler.fit_transform(testX)
+
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 # print("scaled training x[-1], y[-1]", trainX[-1], trainY[-1])
 print("trainX shape", trainX.shape)
 
-reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.5, patience=5, min_lr=0.0000000001)
-saver = keras.callbacks.ModelCheckpoint(f'hsdbModel0_{timeStr}.h5', monitor='val_acc', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
+reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.75, patience=10, min_lr=0.0000000001)
+saver = keras.callbacks.ModelCheckpoint(f'../models/hsdbDirectionalFFModel0_{timeStr}.h5', monitor='val_acc', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
 #opt = keras.optimizers.Adam(lr=0.0005, epsilon=0.00000001, decay=0.00001, amsgrad=False)
 opt = "Adam"
 K.tensorflow_backend._get_available_gpus()
@@ -117,7 +181,15 @@ model.add(Dense(Din, activation='relu'))
 model.add(Flatten())
 model.add(Dense(1, activation='linear'))
 model.compile(loss="mse", optimizer=opt, metrics=['accuracy'])
-model.fit(trainX, trainY, nb_epoch=100, batch_size=10, verbose=1, callbacks=[reduce_lr, saver])
+model.fit_generator(hsdbSequence(trainX, trainY, b), steps_per_epoch=(len(trainX) / b),
+                                          epochs=nb_epoch,
+                                          verbose=2,
+                                          validation_data=hsdbSequence(testX, testY, b),
+                                          validation_steps=(len(testX) / b),
+                                          use_multiprocessing=False,
+                                          workers=4,
+                                          max_queue_size=4)
+# model.fit(trainX, trainY, nb_epoch=nb_epoch, batch_size=5, verbose=2, callbacks=[reduce_lr, saver])
 
 # FOR UNIDIMENTIONAL PREDICTIONS VVV
 
@@ -128,8 +200,9 @@ for i in range(len(testX)):
         passes += 1
     else:
         fails += 1
+    Ps.append(pY)
     errs.append(abs(pY - rY)/max([pY, rY]) * 100)
     print("sTXi:", sTXi)
     print("pY:", pY, "rY:", rY, "err %:", errs[-1])
 
-print("\n\nAggregate Binary Accuracy:", passes, "/", len(testY), "ABA%:", passes / len(testY), "Mean % Error:", np.mean(errs))
+print("\n\nAggregate Binary Accuracy:", passes, "/", len(testY), "ABA%:", passes / len(testY), "Mean % Error:", np.mean(errs), "Mean Pred.:", np.mean(Ps))
